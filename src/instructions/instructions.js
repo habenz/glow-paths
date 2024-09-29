2f263c88270c137f3847cb47f8185c096aed23cb
habenz <haben.zaidt@gmail.com>
2024-09-29 00:26:11 -0400

    make the buttons open and close the modal

--- src/instructions/instructions.js
+++ src/instructions/instructions.js
@@ -12,6 +12,16 @@ class Modal extends HTMLElement {
 
     const shadowRoot = this.attachShadow({ mode: "open" });
     shadowRoot.appendChild(container);
+
+    const buttons = this.shadowRoot.querySelectorAll("button");
+    for (const b of buttons) {
+      b.addEventListener("click", () => this._toggleVisibility());
+    }
+  }
+
+  _toggleVisibility() {
+    const modal = this.shadowRoot.querySelector(".wrapper");
+    modal.classList.toggle("visible");
   }
 }
 
